cf55d511fa9e6c4b422c70b65f57071f3bd49fcc
adityabatman101 <aditya.shylesh@hotmail.com>
2018-05-17 20:30:59 +0400

    Updated simplea.py csvemail.py

--- simplea/simplea.py
+++ simplea/simplea.py
@@ -1,30 +1,66 @@
-from module1 import *
+import sys
 
-filename_provided = 'cnaj.csv'
+import getpass 
 
-EMAIL_SUBJECT = 'This is some subject'
+from csvemail import *
 
-EMAIL_TEXT = '<p>Hello {}<h1>This is some title</h1><a href=\'github.com/adishy\'>This is a link in the email</a>'
+#EXAMPLE: <executable_path> somedetails.csv email_content.html "Some text" aditya.shylesh@hotmail.com 0 1 2
 
-email_sender = Emailer('aditya.shylesh@hotmail.com', 
-					   '1123581321345589_$RRs')
+if len(sys.argv) != 8:
+	print('Usage: <executable> \
+				  <CSV-FILE> \
+				  <EMAIL-CONTENT-FILE> \
+				  <EMAIL-SUBJECT-STRING> \
+				  <EMAIL-SENDER-ADDRESS> \
+				  <RECIPIENT-EMAIL-COLUMN> \
+				  <RECIPIENT-NAME-COLUMN> \
+				  <RECIPIENT-ATTACHMENT-PATH-COLUMN>')
 
-EMAIL_COLUMN = 0
 
-NAME_COLUMN = 1
+filename_provided = sys.argv[1]
 
-ATTACHMENTS_COLUMN = 2
+EMAIL_TEXT_FILENAME = sys.argv[2]
 
-def get_email_data(some_given_text):
-	return str.format(EMAIL_TEXT, some_given_text)
+EMAIL_SUBJECT = sys.argv[3]
+
+EMAIL_SENDER_ADDRESS = sys.argv[4]
+
+EMAIL_TEXT = 'Text'
+
+try:
+	EMAIL_TEXT_FILE = open(EMAIL_TEXT_FILENAME, 'r')
+	EMAIL_TEXT = EMAIL_TEXT_FILE.read()
+
+except:
+	print('Could not open file: ' + EMAIL_TEXT_FILENAME)
+	exit(1)
+
+print("Login for: "  + EMAIL_SENDER_ADDRESS)
+
+EMAIL_SENDER_PASSWORD = getpass.getpass()
+
+email_sender = Emailer(EMAIL_SENDER_ADDRESS, 
+					   EMAIL_SENDER_PASSWORD)
+
+EMAIL_COLUMN = int(sys.argv[5])
+
+NAME_COLUMN = int(sys.argv[6])
+
+ATTACHMENTS_COLUMN = int(sys.argv[7])
+
+COLUMN_SUBSTITUTION = '<employee-name>'
+
+ROWS_IN_FILE_PROVIDED = 0
 
 def process_row_from_file_provided(some_row_provided):
+	globals()['ROWS_IN_FILE_PROVIDED'] = globals()['ROWS_IN_FILE_PROVIDED'] + 1
+
 	email_sender.send(EmailDetails(some_row_provided[EMAIL_COLUMN], 
 								   EMAIL_SUBJECT,
-								   get_email_data(some_row_provided[NAME_COLUMN]), 
+								   EMAIL_TEXT.replace(COLUMN_SUBSTITUTION, some_row_provided[NAME_COLUMN]), 
 								   some_row_provided[ATTACHMENTS_COLUMN].split(",")))
 		
 
 read_csv_file(filename_provided, process_row_from_file_provided)
 
-print('Sent to recipients in file: ' + filename_provided)
+print('Sent to: ' + str(ROWS_IN_FILE_PROVIDED) + ' recipients in file: ' + filename_provided)
