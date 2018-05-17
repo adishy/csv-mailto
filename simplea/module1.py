4bddba008ead6130acd2510a51d12df0b11bced2
adityabatman101 <aditya.shylesh@hotmail.com>
2018-05-17 12:56:05 +0400

    Modified simplea.py module1.py

---
+++ simplea/module1.py
@@ -0,0 +1,107 @@
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from email.mime.base import MIMEBase
+from email import encoders
+ 
+class EmailDetails:
+    def __init__(self, 
+                 receiver_email_address_provided, 
+                 subject_text_provided, 
+                 email_data_provided,
+                 filepaths_provided):
+        self.receiver_email_address = receiver_email_address_provided
+        self.subject_text = subject_text_provided
+        self.email_data = email_data_provided
+        self.filepaths = filepaths_provided
+
+class Emailer:
+    def __del__(self):
+        self.server.quit()
+
+    def __init__(self, 
+                sender_email_provided, 
+                sender_password_provided, 
+                email_list_provided = [], 
+                smtp_server_provided = 'smtp-mail.outlook.com:587'):
+        self.sender_email = sender_email_provided
+        self.sender_password = sender_password_provided
+        self.email_list = email_list_provided
+        self.smtp_server = smtp_server_provided
+
+        self.server = smtplib.SMTP(self.smtp_server)
+        self.server.starttls()
+        self.server.login(self.sender_email, 
+                          self.sender_password)
+
+    def create_msg_string(self, some_email_provided):
+        msg = MIMEMultipart()
+        msg['From'] = self.sender_email
+        msg['To'] = some_email_provided.receiver_email_address
+        msg['Subject'] = some_email_provided.subject_text
+
+        #while(len(some_email_provided.filepaths)):
+        #    some_file = open(some_email_provided.filepaths[len(some_email_provided.filepaths) -1], 'rb')
+        #    some_attach_data = MIMEText(some_file.read(), )
+        #    some_file.close()
+        #    msg.attach(some_attach_data)
+
+        msg.attach(MIMEText(some_email_provided.email_data, 'plain'))
+
+        return msg.as_string()
+
+    def send(self, some_email_provided):
+        self.server.sendmail(self.sender_email, 
+                             some_email_provided.receiver_email_address, 
+                             self.create_msg_string(some_email_provided))
+
+    def add_email(self, some_email_provided):
+        self.email_list.append(some_email_provided)
+
+
+    def send_emails(self):
+
+        while(True):
+            while(len(self.email_list)):
+                self.send(self.email_list[len(self.email_list) - 1])
+                self.email_list.pop()
+
+class EmailProvided:
+    def __init__(self, 
+                 sender_email_address_provided, 
+                 sender_password_provided, 
+                 receiver_email_address_provided, 
+                 subject_text_provided, 
+                 email_data_provided, 
+                 filepaths_provided, 
+                 smtp_server_provided = 'smtp-mail.outlook.com:587'):
+        self.sender_email_address = sender_email_address_provided
+        self.sender_email_password = sender_password_provided
+        self.receiver_email_address = receiver_email_address_provided
+        self.subject_text = subject_text_provided
+        self.email_data = email_data_provided
+        self.filepaths = filepaths_provided
+        self.SMTP_server = smtp_server_provided
+
+        server = smtplib.SMTP(self.SMTP_server)
+        server.starttls()
+        server.login(self.sender_email_address, 
+                     self.sender_email_password)
+
+    def create_msg_string(self):
+        msg = MIMEMultipart()
+        msg['From'] = self.sender_email_address
+        msg['To'] = self.receiver_email_address
+        msg['Subject'] = self.subject_text
+        msg.attach(MIMEText(self.email_data, 'plain'))
+
+        return msg.as_string()
+
+    def sendEmail(self):
+        
+        server.sendmail(self.sender_email_address, 
+                        self.receiver_email_address, 
+                        self.create_msg_string())
+
+        server.quit()
+
--- simplea/simplea.py
+++ simplea/simplea.py
@@ -1,13 +1,58 @@
 import csv
 
-try:
-	somefile = open('cnaj.csv', 'r')
+from threading import Thread
 
-	read_csv_provided = csv.reader(somefile)
+from module1 import *
 
-	for some_row in read_csv_provided:
-		for a in some_row:
-			print(a);
 
-except FileNotFoundError:
-	print("File not found in specified path");
+
+filename_provided = 'cnaj.csv'
+
+email_subject = 'Some text here'
+
+text = 'Hello {}, Some text'
+
+email_sender = Emailer("aditya.shylesh@hotmail.com", 
+				"1123581321345589_$RRs")
+
+send_emails_from_details = Thread(target = email_sender.send_emails, 
+			   args = ())
+
+send_emails_from_details.start()
+
+def get_email_data(some_given_text):
+	return str.format(text, some_given_text)
+
+def process_row_from_file_provided(some_row_provided):
+	email_sender.add_email(EmailDetails(some_row_provided[0], 
+										email_subject, 
+										get_email_data(some_row_provided[1]), 
+										[some_row_provided[2]]))
+
+def read_csv_file(filename_provided, some_callback_provided):
+	try:
+		somefile = open(filename_provided, 'r')
+
+		read_csv_provided = csv.reader(somefile)
+
+		for some_row in read_csv_provided:
+			some_callback_provided(some_row)
+
+	except FileNotFoundError:
+		print("File not found in specified path");
+		
+
+
+
+#jsnvdjks = EmailProvided("aditya.shylesh@hotmail.com", 
+#					     "1123581321345589_$RRs", 
+#					     "aditya.manjushashylesh@gmail.com", 
+#					     "bksdjf", 
+#					     "kjdsgbskdjgb", 
+#					     "")
+
+#jsnvdjks.sendEmail()
+
+#fnjds.add_email(EmailDetails("aditya.manjushashylesh@gmail.com", "jndskfnsd", "fknskdjfnksjd", ['kjsdv.pdf']))
+
+read_csv_file(filename_provided, process_row_from_file_provided)
